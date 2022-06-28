be988cc1d2b69baaaefcc6b04a6d36ce2f7e6a26
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-27 20:07:02 -0500

    Update Main class
    testing connection to the database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-practice", "root", "kingslayer");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from people");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstname"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
